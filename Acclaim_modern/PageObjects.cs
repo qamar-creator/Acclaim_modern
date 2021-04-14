c35720accd1a1bfa08cb0e04d82fc1160ebfae37
ulzaman <ulzaman415@gmail.com>
2021-04-14 16:28:58 +0530

    Add project files.

---
+++ Acclaim_modern/PageObjects.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Protractor;
+
+namespace Acclaim_modern
+{
+    class PageObjects
+    {
+
+        public static string productprice()
+        {
+            string Modelforprice = "productPrice";
+
+            return Modelforprice;
+
+        }
+
+
+        public static string discountonproduct(string which_object)
+        {
+
+            string Modelfordiscount = "discountPercent";
+            string page_heading = "//h3[normalize-space()='Shopping Discount']";
+
+            switch (which_object)
+            {
+                case "page_heading":
+
+                    return page_heading;
+                    break;
+
+                case "discountPercent":
+                    return Modelfordiscount;
+                    break;
+
+
+            }
+
+
+            return which_object;
+
+        }
+
+
+        public static string savedAmount()
+        {
+
+            string ModelsavedAmount = "#savedAmount";
+            return ModelsavedAmount;
+        }
+    }
+}
---
+++ Acclaim_modern/Steps/_1LoginSteps.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+  
+namespace Acclaim_modern
+{
+    [Binding]
+    public class Login
+    {
+        [Given(@"go to this url ""(.*)""")]
+        public static void GivenGoToThisUrl(string url)
+        {
+            SETUP setup = new SETUP();
+            setup.TestMethodLogin(url);
+
+        }
+
+        [Given(@"enter username and password")]
+        public void GivenEnterUsernameAndPassword()
+        {
+        }
+
+        [Given(@"I enter as below")]
+        public static void GivenIEnterAsBelow(Table credentials)
+        {
+            dynamic Credentials = credentials.CreateDynamicInstance();
+            string username = Credentials.username;
+            string password = Credentials.password;
+            CustomControls.Entertext("#txtLoginId", username, propertytype.CssSelector);
+            CustomControls.Entertext("#txtPassword", password, propertytype.CssSelector);
+
+        }
+
+
+
+        [Given(@"the user should stay on the same login screen\.")]
+        public static void GivenTheUserShouldStayOnTheSameLoginScreen_()
+        {
+            CustomControls.click("#btnLogin", propertytype.CssSelector);
+            delayfor.delay();
+        }
+
+        [Given(@"the user should land on page titles ""(.*)""")]
+        public void GivenTheUserShouldLandOnPageTitles(string dashboard)
+        {
+            CustomControls.click("#btnLogin", propertytype.CssSelector);
+            delayfor.delay();
+
+            Globalelements.Expectedresult = dashboard;
+            Globalelements.Actualresult = CustomControlGets.GettextfromLabel("div[class='col-lg-7'] h2", propertytype.CssSelector);
+            Console.WriteLine("The landed page lands on " + Globalelements.Actualresult);
+            Assertions.assertionequals(Globalelements.Actualresult, Globalelements.Expectedresult);
+        }
+
+    }
+}
